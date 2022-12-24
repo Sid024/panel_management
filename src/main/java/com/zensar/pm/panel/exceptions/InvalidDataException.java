4e88176c14ef45f65d093e449d311dfb1319463f
pavan891970 <119672160+pavan891970@users.noreply.github.com>
2022-12-24 14:54:35 +0530

    Create InvalidDataException.java

---
+++ src/main/java/com/zensar/pm/panel/exceptions/InvalidDataException.java
@@ -0,0 +1,24 @@
+package com.zensar.pm.panel.exceptions;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class InvalidDataException extends RuntimeException{
+
+    private String message;
+
+ 
+
+    public InvalidDataException(String message) {
+        super(message);
+        this.message=message;
+        // TODO Auto-generated constructor stub
+    }
+
+ 
+
+ 
+
+}
