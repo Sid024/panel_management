7f28896f62f9a57722850ef0ffb6d51afb90aab3
VM75201 <VM75201@POOLW42SLPC7905.ind.zensar.com>
2023-01-06 18:10:14 +0530

    commited minor changes

--- src/main/java/com/zensar/pm/panel/repository/InterviewTypeRepository.java
+++ src/main/java/com/zensar/pm/panel/repository/InterviewTypeRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.zensar.pm.panel.entity.InterviewTypesEntity;
 
-public interface InterviewTypeRepository  extends JpaRepository<InterviewTypesEntity, String>{
+public interface InterviewTypeRepository  extends JpaRepository<InterviewTypesEntity, Integer>{
 	
 	
 
